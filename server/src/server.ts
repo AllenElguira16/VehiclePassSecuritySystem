6fe7f1bea42f6a73e6784b012be00e5d859f61b3
AllenElguira16 <46128658+AllenElguira16@users.noreply.github.com>
2019-08-08 20:14:14 +0800

    some changes in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,40 @@
 import express from 'express'
-import { Server as overnightjsServer} from '@overnightjs/core'
 import controllers from './controllers';
 import cors from 'cors';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import connectMongo from 'connect-mongo';
+import { Server as overnightjsServer} from '@overnightjs/core'
 
 class Server extends overnightjsServer{
   constructor() {
-    super()
-    this.app.use(express.urlencoded({extended: true}))
-    this.app.use(express.json())
+    super();
+    let mongoInstance = connectMongo(session);
+    mongoose.connect(this.mongoKey);
+    this.use(mongoInstance);
+    this.app.set("json spaces", 2);
+    super.addControllers(controllers);
+  }
+  
+  private mongoKey = 'mongodb+srv://user:user@clustersofstars-renyu.mongodb.net/socialapp?retryWrites=true&w=majority';
+
+  private use(mongoInstance: any) {
+    this.app.use(express.urlencoded({extended: true}));
+    this.app.use(express.json());
     this.app.use(cors({
       origin: ['http://192.168.100.5:19000', 'http://192.168.100.5:3000'],
       methods: ['GET', 'POST', 'DELETE', 'PUT'],
       credentials: true,
       optionsSuccessStatus: 200
-    }))
-    this.app.set("json spaces", 2)
-    super.addControllers(controllers)
+    }));
+    this.app.use(session({
+      secret: 'the-greatest-secret-key',
+      resave: false,
+      saveUninitialized: true,
+      store: new mongoInstance({
+        mongooseConnection: mongoose.connection
+      })
+    }));
   }
 
   public start(port: Number) {
