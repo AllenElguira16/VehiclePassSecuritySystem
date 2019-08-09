b3f18a6ce07d6a59d408bb9af493b61bceaca9ae
AllenElguira16 <46128658+AllenElguira16@users.noreply.github.com>
2019-08-09 21:30:22 +0800

    some changes

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -1,5 +1,6 @@
 import { Schema, Document, model } from 'mongoose';
 
-export default model('user', new Schema({
-
+export default model<iUser & Document>('user', new Schema<iUser>({
+  username: { type: String },
+  password: { type: String }
 }));
