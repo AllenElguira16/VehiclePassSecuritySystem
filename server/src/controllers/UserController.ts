69d142e9cb51a27f04e39e5f756036bca33e18fa
AllenElguira16 <46128658+AllenElguira16@users.noreply.github.com>
2019-08-11 12:31:59 +0800

    Changes

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,29 +1,41 @@
 import { Request, Response } from "express";
 import { Controller, Get, Post } from "@overnightjs/core";
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
+import UserModel from "../Models/UserModel";
 
 @Controller("api/user")
 class UserController {
   @Get()
   public getUser(request: Request, response: Response) {
-    response.send("Sent!");
+    if (request.session) {
+      if (request.session.user)
+        return response.json({ ...request.session.user });
+    }
+    return response.json({ error: true });
   }
 
   @Post()
-  public signIn(request: Request, response: Response) {
+  public async signIn(request: Request, response: Response) {
     const { username, password }: IUser = request.body;
-    hash(password, 10, (error, hash) => {
-      if (error) {
-        return response.json({ error });
-      }
-      const { save } = new UserModel({ username, password: hash });
-      save(error => {
-        if (error) {
-          return response.json({ error });
-        }
-        return response.send({ success: true });
+    const user = await UserModel.findOne({ username });
+    if (!user) return response.json({ error: true });
+    const isMatch = await compare(password, user.password);
+    if (!isMatch) return response.json({ error: true });
+    if (request.session) {
+      request.session.user = user;
+      request.session.save(error => {
+        if (error) return response.json({ error });
+        return response.json({ success: true });
       });
-    });
+    }
+    // hash(password, 10, (error, hash) => {
+    //   if (error) return response.json({ error });
+    //   const userModel = new UserModel({ username, password: hash });
+    //   userModel.save((error: any) => {
+    //     if (error) return response.json({ error });
+    //     return response.send({ success: true });
+    //   });
+    // });
   }
 }
 
