d557a0f77309ba4bdb1b3a4eb6c4705192fbe4b4
Allen Elguira <AllenElguira16@gmail.com>
2019-08-30 12:10:14 +0800

    Update Socket.ts

--- server/src/Services/Socket.ts
+++ server/src/Services/Socket.ts
@@ -15,4 +15,22 @@ export class MySocketService {
   public newVehicle() {
     if (this.nsp) this.nsp.emit("newVehicle");
   }
+
+  /**
+   * Triggered the namespace is created
+   */
+  public $onNamespaceInit(nsp: SocketIO.Namespace) {}
+
+  /**
+   * Triggered when a new client connects to the Namespace.
+   */
+  public $onConnection(
+    @Socket socket: SocketIO.Socket,
+    @SocketSession session: SocketSession
+  ) {}
+
+  /**
+   * Triggered when a client disconnects from the Namespace.
+   */
+  public $onDisconnect(@Socket socket: SocketIO.Socket) {}
 }
