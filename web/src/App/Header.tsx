ce2dab85719c8394e717971578256b72b4f3f12c
Allen Elguira <AllenElguira16@gmail.com>
2019-09-11 13:10:28 +0800

    Nice changes

--- web/src/App/Header.tsx
+++ web/src/App/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap'
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, Container } from 'reactstrap'
 import { Link, withRouter, RouteComponentProps } from 'react-router-dom'
 import logo from 'Assets/images/LNULogo.webp'
 
@@ -27,35 +27,37 @@ const Header: React.FC<RouteComponentProps> = props => {
       id="header"
       light
     >
-      <NavbarBrand tag={Link} to="/" className="d-flex align-items-center" tabIndex={-1}>
-        <img src={logo} alt="Lyceum-Northwestern University" />
-        <span className="ml-2 d-none d-sm-inline">Vehicle Pass Security System</span>
-      </NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink tag={Link} to="/" tabIndex={-1} active={isActive(/\/$/)}>
-              Home
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/dashboard" tabIndex={-1} active={isActive('dashboard')}>
-              Dashboard
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/vehicle-pass" tabIndex={-1} active={isActive('vehicle-pass')}>
-              Vehicle Pass
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/about-us" tabIndex={-1} active={isActive('about-us')}>
-              About Us
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
+      <Container>
+        <NavbarBrand tag={Link} to="/" className="d-flex align-items-center" tabIndex={-1}>
+          <img src={logo} alt="Lyceum-Northwestern University" />
+          <span className="ml-2 d-none d-sm-inline">Vehicle Pass Security System</span>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/" tabIndex={-1} active={isActive(/\/$/)}>
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/dashboard" tabIndex={-1} active={isActive('dashboard')}>
+                Dashboard
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/vehicle-pass" tabIndex={-1} active={isActive('vehicle-pass')}>
+                Vehicle Pass
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/about-us" tabIndex={-1} active={isActive('about-us')}>
+                About Us
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
     </Navbar>
   )
 }
