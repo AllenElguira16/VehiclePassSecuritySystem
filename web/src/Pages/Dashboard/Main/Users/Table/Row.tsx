bfc1d03cacd441201d685f22d2a08d57bcab20ab
AllenElguira16 <46128658+AllenElguira16@users.noreply.github.com>
2019-10-20 18:18:47 +0800

    Update Row.tsx

--- web/src/Pages/Dashboard/Main/Users/Table/Row.tsx
+++ web/src/Pages/Dashboard/Main/Users/Table/Row.tsx
@@ -4,13 +4,14 @@ import { Edit, Delete } from '@material-ui/icons'
 import { UsersState } from './state'
 import { observer } from 'mobx-react-lite'
 import TableProgressBar from 'Components/Common/TableProgressBar'
-// import LNULogo from 'Reactstrap/Assets/images/LNULogo.webp'
 import Forms from './Forms'
 import { FormState } from 'type'
 
 const UserRows: FC = () => {
   const { userState, formState } = useContext(UsersState)
   const { users, isLoading, rowsPerPage, page } = userState
+  const totalRowsPerPage = page * rowsPerPage
+  const totalUsersKey = page * rowsPerPage + rowsPerPage
 
   const formatDate = (date: Date | null): string => {
     return new Date(date as Date).toLocaleDateString()
@@ -25,31 +26,29 @@ const UserRows: FC = () => {
   return (
     <>
       {users.length !== 0 ? (
-        users
-          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-          .map((user, i) =>
-            formState.currentKey === i ? (
-              <Forms type={formState.type} user={user} key={i} />
-            ) : (
-              <TableRow key={i}>
-                <TableCell>
-                  <IconButton onClick={onClick('edit', i)}>
-                    <Edit />
-                  </IconButton>
-                  <IconButton onClick={onClick('delete', i)}>
-                    <Delete />
-                  </IconButton>
-                </TableCell>
-                <TableCell align="right">{user.firstname}</TableCell>
-                <TableCell align="right">{user.lastname}</TableCell>
-                <TableCell align="right">{user.type}</TableCell>
-                <TableCell align="right">{user.licenseId}</TableCell>
-                <TableCell align="right">
-                  {formatDate(user.dateCreated)}
-                </TableCell>
-              </TableRow>
-            ),
-          )
+        users.slice(totalRowsPerPage, totalUsersKey).map((user, i) =>
+          formState.currentKey === i ? (
+            <Forms type={formState.type} user={user} key={i} />
+          ) : (
+            <TableRow key={i}>
+              <TableCell>
+                <IconButton onClick={onClick('edit', i)}>
+                  <Edit />
+                </IconButton>
+                <IconButton onClick={onClick('delete', i)}>
+                  <Delete />
+                </IconButton>
+              </TableCell>
+              <TableCell align="right">{user.firstname}</TableCell>
+              <TableCell align="right">{user.lastname}</TableCell>
+              <TableCell align="right">{user.type}</TableCell>
+              <TableCell align="right">{user.licenseId}</TableCell>
+              <TableCell align="right">
+                {formatDate(user.dateCreated)}
+              </TableCell>
+            </TableRow>
+          ),
+        )
       ) : (
         <TableRow>
           <TableCell colSpan={6} align="center">
