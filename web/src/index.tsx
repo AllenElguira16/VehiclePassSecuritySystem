cbd119b06bf5ed3711e7a57e5b17847372b3d727
Allen Elguira <AllenElguira16@gmail.com>
2020-01-05 19:27:39 +0800

    Update index.tsx

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,11 +5,8 @@ import * as serviceWorker from './serviceWorker'
 import Axios from 'axios'
 
 const { NODE_ENV } = process.env
-const isProd = NODE_ENV === 'production'
 // Axios.defaults.baseURL = 'http://192.168.100.5:8000'
-Axios.defaults.baseURL = isProd
-  ? 'https://vpss-server.herokuapp.com'
-  : 'http://localhost:8000'
+Axios.defaults.baseURL = 'http://localhost:8000'
 
 Axios.defaults.withCredentials = true
 
